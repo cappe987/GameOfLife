c588df8287054868241c706faccf2cc0d6514a2a
Casper Andersson <andersson.casper99@gmail.com>
2019-03-30 21:03:37 +0100

    Initial Commit

---
+++ GameOfLife/Board.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace GameOfLife
+{
+    public class Board
+    {
+        Form1 form;
+        Cell[,] matrix;
+        public readonly int Size;
+        int cellSize = 20;
+
+        /* HashSet<Cell> Living = new HashSet<Cell>();
+         * Keep a set of all living cells. Only check them 
+         * and their neighbours on Tick().*/
+
+        Random r = new Random();
+
+        public Board(int size, Form1 form){
+
+            this.form = form;
+            form.Paint += new PaintEventHandler(Draw); /////
+            Size = size;
+            matrix = new Cell[size, size];
+            for(int y = 0; y < Size; y++){
+                for(int x = 0; x < Size; x++){
+                    var s = r.Next(0, 3) == 0 ? Cell.State.Alive : Cell.State.Dead;
+                    var c = new Cell(y, x, cellSize, s);
+                    //form.Controls.Add(c);
+                    matrix[y, x] = c;
+                }
+            }
+        }
+
+        public Cell this[int y, int x]{
+            get => matrix[y, x];
+            set => matrix[y, x] = value;
+        }
+        public Cell this[Point p]{
+            get => matrix[p.Y, p.X];
+            set => matrix[p.Y, p.X] = value;
+        }
+
+        public void Tick(){
+            for(int i = 0; i < Size; i++){
+                for(int j = 0; j < Size; j++){
+                    matrix[i, j].NextGen(this);
+                }
+            }
+            for (int i = 0; i < Size; i++){
+                for (int j = 0; j < Size; j++){
+                    matrix[i, j].Tick(this);
+                }
+            }
+        }
+
+        public void Draw(object obj, PaintEventArgs args){ //////////
+            for (int i = 0; i < Size; i++){
+                for (int j = 0; j < Size; j++){
+                    matrix[i, j].Draw(args.Graphics);
+                }
+            }
+        }
+
+        public IEnumerator GetEnumerator() => matrix.GetEnumerator();
+    }
+}
---
+++ GameOfLife/Game.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace GameOfLife
+{
+
+    public class Game{
+        Form1 form;
+        Board board;
+        Timer timer;
+        public Game(){
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            form = new Form1();
+            board = new Board(20, form);
+
+            var b = new Button();
+            b.Location = new Point(500, 500);
+            b.Text = "Next Gen";
+            b.Click += new EventHandler(Next);
+            form.Controls.Add(b);
+
+            timer = new Timer();
+            timer.Tick += new EventHandler(Next);
+            timer.Interval = 1000 /30;
+        }
+
+        public void Run(){
+            timer.Start();
+            Application.Run(form);
+        }
+
+
+
+        private void Next(object sender, EventArgs e){
+            board.Tick();
+            form.Refresh();
+        }
+    }
+}
